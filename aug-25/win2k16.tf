5df10221407810070ca1a31b71838971ce3ec66a
nonylinux <88217517+nonylinux@users.noreply.github.com>
2021-08-28 15:31:08 -0400

    iam_profile aded

--- aug-25/win2k16.tf
+++ aug-25/win2k16.tf
@@ -6,11 +6,12 @@ resource "aws_instance" "windows2016" {
   instance_type          = each.value.instance_type
   subnet_id              = each.value.subnet_id
   key_name               = each.value.key_name
+  iam_instance_profile   = each.value.iam_instance_profile
   vpc_security_group_ids = ["${each.value.sg_private_id}"]
 
   root_block_device {
     volume_size           = 50
-    volume_type           = "standard"
+    volume_type           = "gp2"
     encrypted             = true
     delete_on_termination = true
   }
